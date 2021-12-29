ce74f8cf7d780faa498f098f0b7cafe727131b07
Jeff Snyder <tc.photon@gmail.com>
2021-12-29 16:59:50 -0500

    protobuff added for JS and GO

--- poker-frontend/src/App.vue
+++ poker-frontend/src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Poker"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Poker"/> -->
+  <Poker msg="Poker"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Poker from './components/Poker.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Poker
   }
 }
 </script>
